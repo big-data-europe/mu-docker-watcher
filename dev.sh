d578ce48c32007c0c0208deaf231f4a8975c6ceb
Esteban <esteban.sastre@tenforce.com>
2017-07-18 10:26:07 +0200

    Revert "Updated dev.sh to new naming"
    This reverts commit cf23c7c1c0c5fc21d8c84692d3ef64806e1ebd19.

--- dev.sh
+++ dev.sh
@@ -6,6 +6,6 @@ docker run --rm -it \
            -v "$PWD"/pcap:/app/pcap \
 	         -v "$PWD"/supervisord.conf:/etc/supervisord.conf \
            --network host \
-           --name mu-docker-watcher-service \
+           --name mu-docker-watcher \
            -e MODE=development \
-           mu-docker-watcher-service
+           mu-docker-watcher
